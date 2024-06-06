12d3902e8e9dd527b0aa9f25855c5b9f331fd885
Siddhi Bodake <117656009+Siddhi-Bodake@users.noreply.github.com>
2024-06-06 05:52:34 +0000

    container config done

---
+++ 01_baiscs/variables.js
@@ -0,0 +1,9 @@
+
+const accID = 101;
+let accPass = "123345";
+let accState;
+let accname = "siddhi";
+
+// Using console.table() with an array of objects to display the variables in table format
+console.table([{ accID, accPass, accState, accname }]);
+console.log(accname);
