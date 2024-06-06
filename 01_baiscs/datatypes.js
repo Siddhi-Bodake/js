98b10f18969911ee6f90e12a475f1af66f0129bb
Siddhi Bodake <117656009+Siddhi-Bodake@users.noreply.github.com>
2024-06-06 06:19:09 +0000

    container config done

---
+++ 01_baiscs/datatypes.js
@@ -0,0 +1,16 @@
+let age = 2
+let name = "Sidd";
+let state;
+
+//number=>2
+//boolean=>true/fale
+//null
+//symbol=>unique
+//object
+
+console.log(state)
+console.log(typeof age)
+console.log(typeof "Sidd");
+console.log(typeof null)
+console.log(typeof Object)
+console.log(typeof undefined)
